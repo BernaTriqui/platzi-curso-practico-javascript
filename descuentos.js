1d5bb0d66929a1d188434efff37b3b58cb6527ba
BernaTriqui <75945406+BernaTriqui@users.noreply.github.com>
2021-07-23 00:37:43 -0500

    se termino el taller de descuento, falta implementar lo de cupones que menciona el reto

--- descuentos.js
+++ descuentos.js
@@ -1,4 +1,4 @@
-const coupons = [
+/**const coupons = [
     "JuanDC_es_Batman",
     "pero_no_le_digas_a_nadie",
     "es_un_secreto",
@@ -26,4 +26,29 @@ switch(couponValue) {
 }
     const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
     const resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+}
+**/
+//const precioOriginal = 100;
+//const descuento = 15;
+
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento ) / 100;
+
+    return precioConDescuento;
+
+}
+
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son: $ " + precioConDescuento;
 }
