40afed7e0a5927db23d8782bd828e8d78fe16c9d
BernaTriqui <75945406+BernaTriqui@users.noreply.github.com>
2021-08-03 11:03:44 -0500

    ultima clase---

---
+++ salarios.js
@@ -0,0 +1,85 @@
+const mexico = [];
+mexico.push({
+    name: "Juanita",
+    salary: 1000,
+});
+
+mexico.push({
+    name: "Adriana",
+    salary: 1800,
+});
+
+mexico.push({
+    name: "Lupe",
+    salary: 2000,
+});
+
+mexico.push({
+    name: "Paola",
+    salary: 3500,
+});
+
+mexico.push({
+    name: "Marcelo",
+    salary: 10000,
+});
+
+mexico.push({
+    name: "Simon",
+    salary: 12000,
+});
+
+mexico.push({
+    name: "Josefina",
+    salary: 34000,
+});
+
+mexico.push({
+    name: "Francisco",
+    salary: 6500,
+});
+
+mexico.push({
+    name: "Brenda",
+    salary: 8700,
+});
+
+mexico.push({
+    name: "Dulce",
+    salary: 9400,
+});
+
+mexico.push({
+    name: "Liz",
+    salary: 32400,
+});
+
+mexico.push({
+    name: "Osmar",
+    salary: 4300,
+});
+
+mexico.push({
+    name: "Jared",
+    salary: 3200,
+});
+
+mexico.push({
+    name: "Luis",
+    salary: 9600,
+});
+
+mexico.push({
+    name: "Berna",
+    salary: 4400,
+});
+
+mexico.push({
+    name: "Mariana",
+    salary: 5500,
+});
+
+mexico.push({
+    name: "Luisa",
+    salary: 8700,
+});
